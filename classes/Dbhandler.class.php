75efe2b7553c531a257b5f57cfa65820e01015ce
Ruthvik27042002 <ruthvik27042002@gmail.com>
2023-12-03 21:38:31 +0530

    first-commit

---
+++ classes/Dbhandler.class.php
@@ -0,0 +1,58 @@
+<?php
+
+class Dbhandler {
+    private $host;
+    private $user;
+    private $pass;
+    private $db;
+    private $port;
+    public $conn;
+
+    public function __construct() {
+        $this->connect();
+    }
+
+    private function connect() {
+        // Azure database credentials
+        $this->host = "ruthvik.mysql.database.azure.com";
+        $this->user = "ruthvik";
+        $this->pass = "Password@123";
+        $this->db = "ogtech";
+        $this->port = 3306;
+
+        // Create connection with SSL options
+        $this->conn = mysqli_init();
+
+        // Set SSL options with the correct path to the CA certificate
+        mysqli_ssl_set($this->conn, NULL, NULL, "C:/Users/ruthv/Documents/amrita documents/sem7/cloudcomputing/Project/db/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+
+        // Establish the connection with SSL options
+        if (!$this->conn->real_connect($this->host, $this->user, $this->pass, $this->db, $this->port, NULL, MYSQLI_CLIENT_SSL)) {
+            die("Connection failed: " . $this->conn->connect_error);
+        }
+
+        return $this->conn;
+    }
+
+    public function conn() {
+        // Azure database credentials
+        $host = "ruthvik.mysql.database.azure.com";
+        $user = "ruthvik";
+        $pass = "Password@123";
+        $db = "ogtech";
+        $port = 3306;
+
+        // Create connection with SSL options
+        $this->conn = mysqli_init();
+
+        // Set SSL options with the correct path to the CA certificate
+        mysqli_ssl_set($this->conn, NULL, NULL, "C:/Users/ruthv/Documents/amrita documents/sem7/cloudcomputing/Project/db/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+
+        // Establish the connection with SSL options
+        if (!$this->conn->real_connect($host, $user, $pass, $db, $port, NULL, MYSQLI_CLIENT_SSL)) {
+            die("Connection failed: " . $this->conn->connect_error);
+        }
+
+        return $this->conn;
+    }
+}
---
+++ connect.php
@@ -0,0 +1,54 @@
+<?php
+$serverName = "ruthvik.mysql.database.azure.com";
+$username = "ruthvik";
+$password = "Password@123"; // Replace with your actual password
+$databaseName = "ogtech";
+$port = 3306;
+
+// Create connection
+$conn = mysqli_init();
+
+// Set SSL options with the correct path to the CA certificate
+mysqli_ssl_set($conn, NULL, NULL, "C:/Users/ruthv/Documents/amrita documents/sem7/cloudcomputing/Project/db/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+
+// Establish the connection with SSL options
+if (!$conn->real_connect($serverName, $username, $password, $databaseName, $port, NULL, MYSQLI_CLIENT_SSL)) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected successfully";
+}
+// Sample data for insertion
+$username = "sample_user";
+$pwd = "sample_password";
+$email = "sample@example.com";
+
+// Hash the password
+$hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
+
+// SQL query for insertion
+$sql = "INSERT INTO Members (Username, Password, Email, PrivilegeLevel, Attempt, RegisteredDate)
+        VALUES (?, ?, ?, ?, ?, ?);";
+
+// Prepare the statement
+$stmt = $conn->prepare($sql);
+
+// Default values
+$privilegeLevel = 0;
+$attempt = 3;
+$registerDate = date("Y-m-d");
+
+// Bind parameters
+$stmt->bind_param("sssiis", $username, $hashedPwd, $email, $privilegeLevel, $attempt, $registerDate);
+
+// Execute the statement
+if ($stmt->execute()) {
+    echo "Record inserted successfully.";
+} else {
+    echo "Error: " . $stmt->error;
+}
+
+// Close the statement and connection
+$stmt->close();
+// Close the connection
+$conn->close();
+?>
